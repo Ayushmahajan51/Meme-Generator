0162fa20e1b1f992ce5d12d08213c724e7f9e354
Ayush Mahajan <ayushmahajan513@gmail.com>
2019-06-24 11:38:20 +0530

    Meme-Generator using React.js
    this is a small app to generate memes using random images using React.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Memegenerator from "./MemeGenerator";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Memegenerator />
+    </div>
+  );
+}
+
+export default App;
